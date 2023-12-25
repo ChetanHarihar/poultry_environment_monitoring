92d74e6707a53c8be894658dfd1d4f3026cb995d
ChetanHarihar <chetanhs11@gmail.com>
2023-12-25 13:01:18 +0530

    modified LDR code

--- sensors_code/ldr.py
+++ sensors_code/ldr.py
@@ -1,40 +1,68 @@
-#!/usr/bin/python
-import spidev
-import time
-import RPi.GPIO as GPIO
+# Import necessary libraries/modules
+import spidev       # SPI (Serial Peripheral Interface) library for communication with MCP3008 ADC
+import time         # Time library for introducing delays
+import RPi.GPIO as GPIO  # GPIO library for Raspberry Pi
 
+# Set the GPIO mode to BCM (Broadcom SOC channel numbering)
 GPIO.setmode(GPIO.BCM)
 
-# Open SPI bus
+# Open SPI bus (Create an SPI object)
 spi = spidev.SpiDev()
-spi.open(0, 0)
-spi.max_speed_hz = 1000000
+spi.open(0, 0)  # Open SPI bus with bus=0 and device=0
+spi.max_speed_hz = 1000000  # Set the SPI bus speed to 1 MHz
 
-# Define custom chip select
+# Define custom chip select (CS) pin for the MCP3008 ADC
 CS_ADC = 12
-GPIO.setup(CS_ADC, GPIO.OUT)
+GPIO.setup(CS_ADC, GPIO.OUT)  # Set the CS_ADC pin as an output
 
 # Function to read SPI data from MCP3008 chip
-def ReadChannel3008(channel):
+def read_channel_3008(channel):
+    """
+    Reads the analog input from the specified channel of the MCP3008 ADC.
+
+    Parameters:
+    - channel: The channel number (0-7) to read from.
+
+    Returns:
+    - The 10-bit ADC value read from the specified channel.
+    """
+    # MCP3008 command format: [start bit, single-ended channel selection, don't care]
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    # Combine the received bytes to get the 10-bit ADC value
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
 # Function to convert ADC value to voltage
-def ConvertToVoltage(value, bitdepth, vref):
-    return vref * (value / (2 ** bitdepth - 1))
+def convert_to_voltage(value, bit_depth, v_ref):
+    """
+    Converts the raw ADC value to voltage.
 
-# Define delay between readings
-delay = 0.5
+    Parameters:
+    - value: The raw ADC value to convert.
+    - bit_depth: The bit depth of the ADC (e.g., 10 bits for MCP3008).
+    - v_ref: The reference voltage of the ADC.
 
+    Returns:
+    - The voltage corresponding to the given ADC value.
+    """
+    return v_ref * (value / (2 ** bit_depth - 1))
+
+# Infinite loop for continuous data reading
 while True:
+    # Enable the MCP3008 ADC by setting the custom chip select (CS_ADC) to low
     GPIO.output(CS_ADC, GPIO.LOW)
-    value = ReadChannel3008(0)  # Assuming the potentiometer is connected to channel 0
-    print(f"Value: {value}")
+    
+    # Read the raw ADC value from channel 0 (assuming LDR is connected to channel 0)
+    raw_value = read_channel_3008(0)
+    
+    # Disable the MCP3008 ADC by setting the custom chip select (CS_ADC) to high
     GPIO.output(CS_ADC, GPIO.HIGH)
-    voltage = ConvertToVoltage(value, 10, 3.3)  # MCP3008 is 10-bit
 
+    print(f"Raw Value: {raw_value}")
+
+    # Convert the raw ADC value to voltage using the MCP3008's 10-bit resolution and 3.3V reference
+    voltage = convert_to_voltage(raw_value, 10, 3.3)
     print(f"Voltage: {voltage:.3f}V")
 
-
-    time.sleep(delay)
+    # Wait for 1 second before the next reading
+    time.sleep(1)
