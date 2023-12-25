aafa2f593a2b76feb4893280ffd8e45e49cb4588
ChetanHarihar <chetanhs11@gmail.com>
2023-12-25 09:46:04 +0530

    modified dht11 code

--- sensors_code/dht11.py
+++ sensors_code/dht11.py
@@ -1,13 +1,24 @@
+# Import the Adafruit_DHT library to work with the DHT11 sensor
 import Adafruit_DHT
+# Import the time module for time-related functions
 import time
- 
+
+# Set the sensor type (DHT11) and the GPIO pin number to which the sensor is connected
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
- 
+
+# Infinite loop to continuously read sensor data
 while True:
+    # Read humidity and temperature from the DHT11 sensor
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+
+    # Check if the sensor readings are valid
     if humidity is not None and temperature is not None:
+        # Print the temperature and humidity in a formatted string
         print("Temperature={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
     else:
+        # Print an error message if there is a sensor failure
         print("Sensor failure. Check wiring.")
-    time.sleep(2)
+
+    # Pause for 1 seconds before the next sensor reading
+    time.sleep(1)
