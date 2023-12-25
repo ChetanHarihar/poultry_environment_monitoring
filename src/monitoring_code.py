a2e4890a4da5d5e50fb4164fc8db2f27930dcac3
ChetanHarihar <chetanhs11@gmail.com>
2023-12-25 18:22:06 +0530

    modified monitoring code

--- src/monitoring_code.py
+++ src/monitoring_code.py
@@ -2,6 +2,7 @@ from mcpadc import MCP  # Import the MCP class from the provided module
 import Adafruit_DHT  # Import the Adafruit_DHT library to work with the DHT11 sensor
 import requests  # Import the requests module for HTTP requests
 import time  # Import the time module for time-related functions
+import RPi.GPIO as GPIO  # Import the RPi.GPIO module for GPIO operations
 
 # Replace with your ThingSpeak API Key and Channel ID
 api_key = "YOUR_API_KEY"
@@ -21,24 +22,24 @@ DHT_PIN = 4
 def read_sensor_values():
     # Read LDR value from channel 0
     ldr_channel = 0
-    GPIO.output(CS_ADC, GPIO.LOW)
+    GPIO.output(adc_module.CS_ADC, GPIO.LOW)
     ldr_raw_value = adc_module.read_channel(ldr_channel)  # Read raw ADC value from LDR channel
-    GPIO.output(CS_ADC, GPIO.HIGH)
-    ldr_voltage = adc_module.convert_to_voltage(ldr_raw_value)  # Convert raw value to voltage
+    GPIO.output(adc_module.CS_ADC, GPIO.HIGH)
+    ldr_voltage = round(adc_module.convert_to_voltage(ldr_raw_value), 2)  # Convert raw value to voltage and round to 2 decimal places
 
     # Read gas sensor 1 value from channel 1
     gas_sen1_channel = 1
-    GPIO.output(CS_ADC, GPIO.LOW)
+    GPIO.output(adc_module.CS_ADC, GPIO.LOW)
     gas_sen1_raw_value = adc_module.read_channel(gas_sen1_channel)  # Read raw ADC value from gas_sen1 channel
-    GPIO.output(CS_ADC, GPIO.HIGH)
-    gas_sen1_voltage = adc_module.convert_to_voltage(gas_sen1_raw_value)  # Convert raw value to voltage
+    GPIO.output(adc_module.CS_ADC, GPIO.HIGH)
+    gas_sen1_voltage = round(adc_module.convert_to_voltage(gas_sen1_raw_value), 2)  # Convert raw value to voltage
 
     # Read gas sensor 2 value from channel 2
     gas_sen2_channel = 2
-    GPIO.output(CS_ADC, GPIO.LOW)
+    GPIO.output(adc_module.CS_ADC, GPIO.LOW)
     gas_sen2_raw_value = adc_module.read_channel(gas_sen2_channel)  # Read raw ADC value from gas_sen2 channel
-    GPIO.output(CS_ADC, GPIO.HIGH)
-    gas_sen2_voltage = adc_module.convert_to_voltage(gas_sen2_raw_value)  # Convert raw value to voltage
+    GPIO.output(adc_module.CS_ADC, GPIO.HIGH)
+    gas_sen2_voltage = round(adc_module.convert_to_voltage(gas_sen2_raw_value), 2)  # Convert raw value to voltage
 
     # Read humidity and temperature from the DHT11 sensor
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
@@ -61,7 +62,7 @@ try:
         print(f"Data sent - Temp: {temp}, Humidity: {humidity}, LDR: {ldr_value}, Gas Sensor 1: {gas1_value}, Gas Sensor 2: {gas2_value}. Response: {response.text}")
         
         # Wait for 15 seconds before sending the next update
-        time.sleep(5)
+        time.sleep(15)
 
 except KeyboardInterrupt:
     print("Script terminated by user.")
